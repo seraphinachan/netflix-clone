60a42f0c4527f69e7e3181238917adcbac151984
Administrator <jiyeonyee0312@gmail.com>
2023-06-06 00:29:31 +0900

    Landing page commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './Screens/HomeScreen';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginScreen from './Screens/LoginScreen';
 
 function App() {
+  const user = null;
+  
   return (
     <div className="app">
-     <HomeScreen />
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        )}
+      </Router>
     </div>
   );
 }
--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./HomeScreen.css";
-import Nav from "./Nav";
-import Banner from "./Banner";
+import Nav from "../Nav";
+import Banner from "../Banner";
 
 function HomeScreen() {
     return (
     <div className="homeScreen">
         <Nav />
-
+        
         <Banner />
 
     </div>
